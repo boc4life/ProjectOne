b26691db596f5f2cdf32d4150dd5b369b4a2a813
boc4life <lheandrew@gmail.com>
2018-12-31 15:32:09 -0500

    notes added, input requested

--- logic.js
+++ logic.js
@@ -1,3 +1,12 @@
+// With our finished product, all of these variables should receive their values through accessing our Firebase database.
+// I believe our best bet is to use localStorage to store the current city the user is viewing.
+// Front end display of restaurant images needs a lot of work. I'm also having difficulty with the text that says "Have a restaurant you'd like to add...", as I can't figure out why it gets pushed to center after the function callback runs.
+// How much more do we want to do with the carousel? Should we make each restaurant clickable? If so, what do we want it to do? Several options...
+// Option One: Bring user to Google Places info page for the restaurant. I'm 99% sure this is possible but not 100.
+// Option Two: Stop carousel, display more info about restaurant somewhere on the page. Might be a bit difficult to implement the logic and design for this.
+// Option Three: Don't make restaurants clickable, just display more restaurant info while the carousel is running.
+// Other options or ideas are welcome and encouraged. I'm just writing out what's coming to my mind now.
+
 var city = "Philadelphia"
 var cityBlurb = "Founded in 1682, Philadelphia is one of the most historic cities in the United States. Visitors can immerse themselves in American history, while exploring a city full of culture and cheesesteaks."
 
--- searchlogic.js
+++ searchlogic.js
@@ -1,3 +1,5 @@
+// Currently this search only returns the top result. Discuss if we want to keep this as is, or if we'd like to return multiple results.
+
 $("#restaurantSearchBtn").on("click", clickSearch);
 
 var city = "philadelphia" 
@@ -25,5 +27,22 @@ function searchCallback (searchResults, status) {
         console.log(searchResults);
         $("#searchResultsContainer").empty();
         $("#searchResultsContainer").html("Do you mean " + searchResults[0].name + " at " + searchResults[0].formatted_address + "?");
+        var yesBtn = $("<button>");
+        yesBtn.addClass("data-ID=" + searchResults[0].place_id);
+        yesBtn.addClass("yesButton");
+        yesBtn.text("Yes")
+        var noBtn = $("<button>");
+        // I currently have this "No" Button rendering. I'm not sold on including it. Would like some thoughts. This kind of goes hand in hand with the 1 result or multiple results discussion.
+        noBtn.addClass("noButton");
+        noBtn.text("No");
+        $("#searchResultsContainer").append(yesBtn);
     }
 }
+
+function addRestaurant() {
+    $("#searchResultsContainer").html("Thank you! Your restaurant has been added to our list!")
+
+    // Add the value of "data-id" on the yes button to the city's Firebase. If a user is logged in, maybe attach their username to be displayed to all visitors (e.g. Jim's - Added by _______)?
+}
+
+$(document).on("click", ".yesBtn", addRestaurant);
