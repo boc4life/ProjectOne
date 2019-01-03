9aeecb3f0a2cc42b76fa5cfc4c1ea1a5253ab169
boc4life <lheandrew@gmail.com>
2019-01-03 18:48:57 -0500

    search result can be added to firebase

--- assets/javascript/searchlogic.js
+++ assets/javascript/searchlogic.js
@@ -6,7 +6,7 @@ var city = "philadelphia"
 var latlng = {lat: 39.9526, lng: -75.1652}
 
 function clickSearch() {
-    $.getScript("http://maps.google.com/maps/api/js?key=MYKEY&libraries=places&callback=restaurantSearch");
+    $.getScript("http://maps.google.com/maps/api/js?key=AIzaSyD7rrcP_wAQd4SZa6nZVTbMsyMQp1v2Ml4&libraries=places&callback=restaurantSearch");
 }
 
 function restaurantSearch() {
@@ -28,20 +28,18 @@ function searchCallback (searchResults, status) {
         $("#searchResultsContainer").empty();
         $("#searchResultsContainer").html("Do you mean " + searchResults[0].name + " at " + searchResults[0].formatted_address + "?");
         var yesBtn = $("<button>");
-        yesBtn.addClass("data-ID=" + searchResults[0].place_id);
+        yesBtn.attr("data-ID", searchResults[0].place_id);
         yesBtn.addClass("yesButton");
-        yesBtn.text("Yes")
-        var noBtn = $("<button>");
-        // I currently have this "No" Button rendering. I'm not sold on including it. Would like some thoughts. This kind of goes hand in hand with the 1 result or multiple results discussion.
-        noBtn.addClass("noButton");
-        noBtn.text("No");
+        yesBtn.text("Yes");
         $("#searchResultsContainer").append(yesBtn);
     }
 }
 
 function addRestaurant() {
     $("#searchResultsContainer").html("Thank you! Your restaurant has been added to our list!")
+    var newRest = $(this).attr("data-ID");
+    console.log(newRest);
+    database.ref(city + "/restaurants").push(newRest);
 
-    // Add the value of "data-id" on the yes button to the city's Firebase. If a user is logged in, maybe attach their username to be displayed to all visitors (e.g. Jim's - Added by _______)?
 }
 
